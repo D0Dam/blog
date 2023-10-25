25bfca4939a13726927f2819c631a300b831a9d7
d0dam <smallkdb@gmail.com>
2023-10-25 17:59:00 +0900

    리팩토링: 메인페이지 블로그로 refirect 임시 코드 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 export default function Home(): JSX.Element {
-	return (
-		<main>
-			<HomepageFeatures />
-		</main>
-	);
+  useEffect(() => {
+    window.location.href = 'https://d0dam.xyz/blog';
+  }, []);
+
+  return (
+    <main>
+      <HomepageFeatures />
+    </main>
+  );
 }

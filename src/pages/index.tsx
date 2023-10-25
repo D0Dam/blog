f800dba1ca2a412393e9b1b64c984f5fde68db85
d0dam <smallkdb@gmail.com>
2023-10-25 21:24:33 +0900

    리팩토링: 메인 페이지 복구

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 export default function Home(): JSX.Element {
-  useEffect(() => {
-    window.location.href = 'https://d0dam.xyz/blog';
-  }, []);
-
   return (
     <main>
       <HomepageFeatures />

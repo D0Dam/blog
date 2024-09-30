983e58d60a1706aca77f6086dfb16be2d4b7912c
d0dam <smallkdb@gmail.com>
2024-09-30 13:05:31 +0900

    기능: 구글 애드센스 스크립트 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Head from '@docusaurus/Head';
 
 export default function Home(): JSX.Element {
   return (
     <Layout>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9737695122539343"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <HomepageFeatures />
     </Layout>
   );
